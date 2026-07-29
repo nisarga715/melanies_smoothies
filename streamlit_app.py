cfbf23542f8a85812834f6d48f516cbec502cec8
Nisarga M U <151115752+nisarga715@users.noreply.github.com>
2026-07-29 10:19:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     time_to_insert = st.button('Submit Order')
 
@@ -44,10 +47,6 @@ if ingredients_list:
 import requests
 import pandas as pd
 
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
